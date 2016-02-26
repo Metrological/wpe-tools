ad94c87272a59c45a6110c3260b60ebf3727a8ee
Guillaume Emont <guijemont@igalia.com>
2016-02-25 21:32:11 -0600

    Added succint help to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ x"$WPET_CONFIG_PARSED" != x"true" ]; then
 fi
 
 # These few lines reorder $@ so that options come first.
-TEMP=`getopt -o d --long deploy -- "$@"`
+TEMP=`getopt -o dh --long deploy,help -- "$@"`
 if [ $? != 0 ] ; then echo "Problem parsing options" >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
 
@@ -16,6 +16,7 @@ DEPLOY=false
 while true ; do
     case "$1" in
         -d|--deploy) DEPLOY=true ; shift ;;
+        -h|--help) echo "syntax: $0 [-d] [-h] [make options]" ; exit 0;;
         --) shift ; break ;;
         *) echo "unknown option: $1" ; exit 1 ;;
     esac
