5360d5a58a0634ad897439c2966d93b1f0a1587b
Guillaume Emont <guijemont@igalia.com>
2016-02-26 19:33:44 -0600

    Debugging: do not tee remote output and improve visual of script messages.
    I had some terminal issues in gdb and it may be improved by completely
    redirecting remote output to a file instead of through tee.

--- debug-browser.sh
+++ debug-browser.sh
@@ -6,7 +6,11 @@ if [ x"$WPET_CONFIG_PARSED" != x"true" ]; then
     source "$TOOLS_DIR/config.sh"
 fi
 
-ssh -p $WPET_REMOTE_SSH_PORT "$WPET_REMOTE_SSH_USER@$WPET_REMOTE_HOST" 'run_wpe(){  /usr/bin/WPELauncher & sleep 1; gdbserver --attach 0.0.0.0:2345 `pidof WPEWebProcess` ; }; run_wpe' |& tee run-wpe.log &
+echo ">>> starting program on remote host"
+ssh -p $WPET_REMOTE_SSH_PORT "$WPET_REMOTE_SSH_USER@$WPET_REMOTE_HOST" 'run_wpe(){  /usr/bin/WPELauncher & sleep 1; gdbserver --attach 0.0.0.0:2345 `pidof WPEWebProcess` ; }; run_wpe' > run-wpe.log 2>&1 &
+
+echo ">>> waiting 1 second"
 sleep 1
 
+echo ">>> starting gdb"
 PYTHONPATH=$PYTHONPATH:$WPET_OUTPUT/host/usr/share/gcc-$WPET_GCC_VERSION/python/ $WPET_OUTPUT/host/usr/bin/$WPET_PLATFORM-gdb --command=webkit_debug.py
