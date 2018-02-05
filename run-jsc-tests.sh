7dcd6b1eb5a4f4aa04b2888684a2297f69d79c77
Guillaume Emont <guijemont@igalia.com>
2018-02-05 17:24:24 -0600

    Specialize file name of outputs + misc improvements

--- run-jsc-tests.sh
+++ run-jsc-tests.sh
@@ -5,12 +5,16 @@ if [ x"$WPET_CONFIG_PARSED" != x"true" ]; then
     TOOLS_DIR=$(dirname ${BASH_SOURCE[0]})
     source "$TOOLS_DIR/config.sh"
 fi
-echo foo
+
+now=`date +'%Y%m%d-%H%M%S'`
+filebase="jsc-tests-$WPET_OUTPUT_NAME-$now"
+logfile="$filebase.log"
+jsonfile="$filebase.json"
 JSCTEST_timeout=1200 $WPET_WPE_BUILD/Tools/Scripts/run-javascriptcore-tests \
-    --no-build --no-fail-fast --json-output=jsc_results.json --release \
+    --no-build --no-fail-fast --json-output=$jsonfile --release \
     --memory-limited --jsc-only \
     --remote=$WPET_REMOTE_SSH_USER@$WPET_REMOTE_HOST \
-	--env-vars TZ='PST8PD' \
+    --env-vars TZ='America/Los_Angeles' \
     $@ \
-	|& tee jsc-tests.log
+	|& tee $logfile
 
